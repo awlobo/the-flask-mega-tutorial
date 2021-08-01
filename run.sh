07807908715eacec44f17ef3436850c03f66eb54
Andrea Lobo <andrea.lobo.arroyo@gmail.com>
2021-08-01 23:51:51 +0200

    Error handling

--- run.sh
+++ run.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 export FLASK_APP=app.py
+export FLASK_ENV=development
+
+UNAME=$(uname)
+
+if [[ "$UNAME" == "Linux" || "$UNAME" == "Darwin" ]] ; then
+	export FLASK_APP=app.py
+    export FLASK_ENV=development
+elif [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]] ; then
+	set FLASK_APP=app.py
+    set FLASK_ENV=development
+fi
+
 flask run
 
 # flask db migrate -m ...
