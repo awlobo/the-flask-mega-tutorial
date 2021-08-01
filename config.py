07af1da31a387187c43e7df7837e92e472c41c95
Andrea Lobo <andrea.lobo.arroyo@gmail.com>
2021-08-01 17:28:38 +0200

    Setting up github actions

--- config.py
+++ config.py
@@ -2,4 +2,7 @@ import os
 
 
 class Config(object):
+    TESTING = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-know'
+    # Disable CSRF protection in the testing configuration
+    WTF_CSRF_ENABLED = False
