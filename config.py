7c48497d7d07eec45efc4d8b6251d9a00153f816
Andrea Lobo <andrea.lobo.arroyo@gmail.com>
2021-08-01 20:37:41 +0200

    Add database support

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    TESTING = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-know'
-    # Disable CSRF protection in the testing configuration
-    WTF_CSRF_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
