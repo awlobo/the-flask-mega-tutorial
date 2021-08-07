94bddff36b751f4a219a7a74a904c84de7fec2c3
Andrea Lobo <andrea.lobo.arroyo@gmail.com>
2021-08-07 20:08:45 +0200

    Add docker support

--- run.sh
+++ run.sh
@@ -34,3 +34,13 @@ flask run
 # pybabel compile -d app/translations
 # Update tranlations
 # (venv) $ pybabel update -i messages.pot -d app/translations
+
+# docker
+# $ docker run --name microblog -d -p 8000:5000 --rm -e SECRET_KEY=my-secret-key \
+#     -e MAIL_SERVER=smtp.googlemail.com -e MAIL_PORT=587 -e MAIL_USE_TLS=true \
+#     -e MAIL_USERNAME=<your-gmail-username> -e MAIL_PASSWORD=<your-gmail-password> \
+#     --link mysql:dbserver \
+#     -e DATABASE_URL=mysql+pymysql://microblog:<database-password>@dbserver/microblog \
+#     --link elasticsearch:elasticsearch \
+#     -e ELASTICSEARCH_URL=http://elasticsearch:9200 \
+#     microblog:latest
